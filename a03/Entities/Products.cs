d9a72f722b65d561a15ce1fc256712f53166c427
Neemias Pereiragit config --global user.email neemias.pereira3@gmail.comgit config --listgit config --global user.name Neemias <neemias.pereira3@gmail.com>
2023-02-24 14:52:39 -0300

    Estudo 01

---
+++ a03/Entities/Products.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace a03.Entities
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Products(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void IcreaseSalary(double percentage) 
+        {
+            Salary += ( Salary / percentage);
+        }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Name
+                +", "
+                + Salary.ToString("f2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ a03/Program.cs
@@ -0,0 +1,49 @@
+﻿using a03.Entities;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace a03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Products> p1 = new List<Products>();
+            Console.Write("How many employee will be registred?");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Emproyee #{0}", i);
+                Console.Write("id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                p1.Add(new Products(id, name, salary));
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter id that will have salary increase: ");
+            int idFind = int.Parse(Console.ReadLine());
+            Products v = p1.Find(x => x.Id == idFind);
+            if (v != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double increase = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                v.IcreaseSalary(increase);
+            }
+            else 
+            {
+                Console.WriteLine("Id does not exist!");
+            }
+
+            Console.WriteLine("Update list of employees: ");
+            foreach (Products obj in p1)
+            {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}
